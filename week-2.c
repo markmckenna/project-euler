a34084c99e0d0bc44eb7632d04f73e85dc79c849
Mark McKenna <mark.mckenna@redspcae.com>
2015-10-02 14:46:37 -0300

    Revision to do better timings

--- week-2.c
+++ week-2.c
@@ -1,8 +1,10 @@
 // Project Euler, week 2
 
+#include <time.h>
 #include <stdio.h>
 
 int main() {
+    clock_t start = clock();
     register int fibMinus2 = 1;
     register int fibMinus1 = 1;
     register int fib = 2;
@@ -12,11 +14,13 @@ int main() {
     const int limit = 4000000;
 
     while (fib < limit) {
-          if (i++ % 2 == 0) sum += fib;
-            fibMinus2 = fibMinus1;
-              fibMinus1 = fib;
-                fib += fibMinus2;
+        if (i++ % 2 == 0) sum += fib;
+        fibMinus2 = fibMinus1;
+        fibMinus1 = fib;
+        fib += fibMinus2;
     }
 
-    printf("Sum: %d", sum);
+    clock_t dur = clock() - start;
+
+    printf("Sum %d in %fms", sum, ((float)dur)/CLOCKS_PER_SEC);
 }
